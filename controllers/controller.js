73d97a39feb1b0fdca01649fb1ef5913a49fa8a8
David <131110984+Dtrieu1@users.noreply.github.com>
2023-09-19 20:41:56 -0500

    setting up MVC

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,1 +1,26 @@
 const router = require('express').Router();
+
+const posts = [
+    {
+        post_title: 'Is this the new apple?',
+        post_description: 'Just curious if anyone who can be the next tech company like apple'
+    },
+    {
+        post_title: 'Why do you think Yahoo got purchased?',
+        post_description: 'Just curious if anyone still uses Yahoo or is that just old technology'
+    }
+]
+
+router.get('/', async (req, res) =>{
+    res.render();
+})
+
+router.get('/login', async (req,res) => {
+    res.render();
+})
+
+router.get('/signup', async(req, res) => {
+    res.render();
+})
+
+module.exports = router;
